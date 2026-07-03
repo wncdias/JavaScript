8642ae35d8f874a4077a0c61da2e1c8ea293d1fb
wncdias <william.nuintin@gmail.com>
2026-07-03 10:21:39 -0300

    revertendo array
    testando reversão de array em nova variável sem usar funções prontas do JS

---
+++ challanges/VeryEasy/LinguagemLimitada.js
@@ -0,0 +1,17 @@
+//Escreva uma função que recebe um array e retorne um novo array com 
+//todas as posições invertidas, do original sem alteralo. 
+//Não utilize os métodos do objeto global. Array do javascript (reverse, map, forEach, etc).
+
+function reverseArray(arr) {
+    if (arr.length === 0) {
+        return [];
+    }
+    let newArray = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        newArray.push(arr[i]);
+    }
+    return newArray;
+}
+
+console.log(reverseArray([1, 2, 3, 4, 5])); // Exemplo de uso da função reverseArray
+console.log(reverseArray(['a', 'b', 'c'])); // Exemplo de uso da função reverseArray
